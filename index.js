351c8c417a318bb827d6e427f0d319b61ee3d823
arnavpanigrahi <43907678+arnavpanigrahi@users.noreply.github.com>
2020-10-14 16:00:27 +0530

    Update index.js
    Added User-Agent to headers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const axios = require('axios').default;
 const client = new Discord.Client();
 const regex = new RegExp(/(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)[^\s]+/gim);
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
 
 client.on('ready', () => {
     console.log(`▓█████▄  ██▓  ██████  ▄████▄   ▒█████   ██▀███  ▓█████▄      ██████  ███▄    █  ██▓ ██▓███  ▓█████  ██▀███
@@ -36,6 +37,7 @@ client.on('message', message => {
                     headers:
                     {
                         'Authorization': process.env.TOKEN
+                        'User-Agent': userAgent
                     }
                 }).then(
                     () => console.log(`No-Scoped a nitro in ${message.guild.name}.`)
